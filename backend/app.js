bf82f120ba6dd051ffa7e30548153350f4c1de60
ThediniSilva <162461392+ThediniSilva@users.noreply.github.com>
2024-04-18 15:05:29 +0530

    Adding Inventory Backend
    Adding Inventory backend

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 
 const offerRouter = require("./routes/offerRoutes");
 const router = require("./routes/TimetableRoutes");
+const InventoryRouter = require("./routes/InventoryRoute");
 
 const app  = express();
 const cors = require('cors');
@@ -14,6 +15,7 @@ app.use(express.json());
 app.use(cors());
 app.use("/offer", offerRouter);
 app.use("/timetables",router);
+app.use("/invetory",InventoryRouter);
 
 
 
