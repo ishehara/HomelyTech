75bda9c327c170684fbcb3ed5d3d0a3597793e60
hasinduW <hasinduweerarathne2001@gmail.com>
2024-04-19 09:46:36 +0530

    Adding feedback backend
    Adding feedback backend

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const router = require("./routes/FeedbackRoutes");
+
 
 
 const offerRouter = require("./routes/offerRoutes");
 const router = require("./routes/TimetableRoutes");
 
 const app  = express();
+const cors = require("cors");
+
 
 //middleware
 app.use(express.json());
@@ -35,3 +39,15 @@ app.listen(5000, () => {
 
 
 
+//Middleware feedback and rating
+app.use(express.json());
+app.use(cors());
+app.use("/feedbacks",router);
+
+mongoose.connect("mongodb+srv://admin:QdEF5HOhFGJU2aww@cluster0.0pwwreq.mongodb.net/")
+
+.then(()=> console.log("Connection to MongoDB"))
+.then(()=>{
+    app.listen(5000);
+})
+.catch((err)=>console.log((err)));
