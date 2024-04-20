930347f6eea6a852bd1e6efcd042d6de50c6f02b
ishehara <mlisliyanage@gmail.com>
2024-04-20 20:20:02 +0530

    update the login

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 
 
 import App from './App';
-import Login from './components/login/login';
+
 import Navbar from './components/customerScreens/navbar';
 import Home from './components/customerScreens/home';
 
