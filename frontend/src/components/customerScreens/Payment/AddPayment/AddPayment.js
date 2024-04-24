cd80f94dd8db2229d7f3f8e4b6a4e8e5ed19c95c
Nethrani Rajapaksha <159509177+NethraniRajapaksha@users.noreply.github.com>
2024-04-24 16:28:37 +0530

    css changes
    frontend make payment
    frontend refund payment
    nav bar css
    footer css

--- frontend/src/components/customerScreens/Payment/AddPayment/AddPayment.js
+++ frontend/src/components/customerScreens/Payment/AddPayment/AddPayment.js
@@ -1,5 +1,8 @@
 import React, { useState } from 'react'; // Corrected import statement
 import './AddPayment.css';
+import Navbar from '../../navbar'
+import Footer from '../../Footer/footer'
+
 // import Nav from '../Nav/nav';
 import { useNavigate } from 'react-router';
 import axios from 'axios';
@@ -46,41 +49,44 @@ function AddPayment() {
     };
 
     return (
-        // <div>
-        //     <Navbar/>
+        <div>
+            <Navbar/>
+            <h1 className='AddPayment-h3'>Take the Next Step by Providing Payment Information to Complete the Order</h1>
         <div>
             
-            <h1 className='AddPayment h1'>Make Payment</h1>
+            
+            <div className='container-payment' >
             <form onSubmit={handleSubmit} className='AddPayment form'>
-                <label className='AddPayment label' htmlFor="fname">Full Name:</label><br />
-                <input type="text" id="fname" name="fname" onChange={handleChange} value={inputs.fname} required/><br />
+                <label className='AddPayment-label' htmlFor="fname">Full Name:</label>
+                <input type="text" id="fname" name="fname" onChange={handleChange} value={inputs.fname} required/>
 
-                <label className='AddPayment label' htmlFor="fname">User ID:</label><br />
-                <input type="text" id="userid" name="userid" onChange={handleChange} value={inputs.userid} required/><br />
+                <label className='AddPayment-label' htmlFor="fname">User ID:</label>
+                <input type="text" id="userid" name="userid" onChange={handleChange} value={inputs.userid} required/>
 
-                <label className='AddPayment label'  htmlFor="gmail">Gmail:</label><br />
-                <input className='AddPayment input[type="email"]' type="email" id="gmail" name="gmail" required onChange={handleChange} value={inputs.gmail} /><br />
+                <label className='AddPayment-label'  htmlFor="gmail">Gmail:</label>
+                <input className='AddPayment input[type="email"]' type="email" id="gmail" name="gmail" required onChange={handleChange} value={inputs.gmail} />
 
-                <label className='AddPayment label'  htmlFor="address">Address:</label><br />
-                <input className='AddPayment input[type="text"]'  type="text" id="address" name="address" required onChange={handleChange} value={inputs.address} /><br />
+                <label className='AddPayment-label'  htmlFor="address">Address:</label>
+                <input className='AddPayment input[type="text"]'  type="text" id="address" name="address" required onChange={handleChange} value={inputs.address} />
 
-                <label className='AddPayment label'  htmlFor="phone">Phone:</label><br />
-                <input  className='AddPayment input[type="tel"]'  type="tel" id="phone" name="Phone" required onChange={handleChange} value={inputs.Phone} /><br />
+                <label className='AddPayment-label'  htmlFor="phone">Phone:</label>
+                <input  className='AddPayment input[type="tel"]'  type="tel" id="phone" name="Phone" required onChange={handleChange} value={inputs.Phone} />
 
-                <label className='AddPayment label'  htmlFor="serviceType">Service Type:</label><br />
-                <input className='AddPayment input[type="text"]'  type="text" id="serviceType "  required name="ServiceType" onChange={handleChange} value={inputs.ServiceType} /><br />
+                <label className='AddPayment-label'  htmlFor="serviceType">Service Type:</label>
+                <input className='AddPayment input[type="text"]'  type="text" id="serviceType "  required name="ServiceType" onChange={handleChange} value={inputs.ServiceType} />
 
-                <label className='AddPayment label'  htmlFor="amount">Amount:</label><br />
-                <input className='AddPayment input[type="number"]' type="number" id="amount" required name="amount" onChange={handleChange} value={inputs.amount} /><br />
+                <label className='AddPayment-label'  htmlFor="amount">Amount:</label>
+                <input className='AddPayment input[type="number"]' type="number" id="amount" required name="amount" onChange={handleChange} value={inputs.amount} />
 
-                <label className='AddPayment label'  htmlFor="paymentSlip">Payment Slip:</label><br />
-                <input className='AddPayment input[type="text"]' type="text" id="paymentSlip" required name="PaymentSlip" onChange={handleChange} value={inputs.PaymentSlip} /><br /><br />
+                <label className='AddPayment-label'  htmlFor="paymentSlip">PaymentSlip Reference Number:</label>
+                <input className='AddPayment input[type="text"]' type="text" id="paymentSlip" required name="PaymentSlip" onChange={handleChange} value={inputs.PaymentSlip} />
 
-                <button type="submit" className='AddPayment button'>Submit</button>
+                <button type="submit" className='AddPayment-button'>Submit</button>
             </form>
             </div>
-        //     <Footer/>
-        // </div>
+            </div>
+            <Footer/> 
+       </div>
     );
 }
 
