94a32bb19bef0811429c1052098cb6859a671f52
ishehara <mlisliyanage@gmail.com>
2024-05-15 16:20:29 +0530

    adding send emails
    its working for all users

--- backend/controllers/offerController.js
+++ backend/controllers/offerController.js
@@ -1,4 +1,10 @@
+const mongoose = require('mongoose');
+
 const offer =  require('../models/offerModel');
+const User = require("../models/UserModel");
+
+const nodemailer = require("nodemailer");
+
 
 //read data
 const getAllOffers = async (req, res, next) => {
@@ -37,6 +43,7 @@ const createOffer = async (req, res, next) => {
 
     try {
         await offers.save();
+        
     }catch(err){
         console.log(err);
     }
@@ -81,6 +88,7 @@ const updateOffer = async (req, res, next) => {
         offers = await offer.findByIdAndUpdate(offerId,
             {title: title, description: description, persentage: persentage, promoCode: promoCode, startDate: startDate, dueDate: dueDate, image: image},);
             offers = await offers.save();
+            
     }catch(err){
         console.log(err);
     }
@@ -119,9 +127,74 @@ const deleteOffer = async (req, res, next) => {
 
 }
 
+// Set up nodemailer transporter
+const transporter = nodemailer.createTransport({
+    service: 'gmail', // Use your email service
+    
+    auth: {
+        user: 'homelytech24@gmail.com', // Replace with your email
+        pass: 'dmjggzwqjquqcign' // Replace with your email password
+    }
+});
+
+const sendMailToUsers = async (offerDetails) => {
+    try {
+        const users = await User.find(); // Fetch all users from the user database
+
+        if (!users || users.length === 0) {
+            console.log("No users found to send emails.");
+            return;
+        }
+
+        users.forEach((user) => {
+            const mailOptions = {
+                from: 'homelytech24@gmail.com', // Replace with your email
+                to: user.gmail, // User email
+                subject: 'New Offer Available!',
+                text: `Hello ${user.fullname},\n\nWe have a new offer for you:\n\nTitle: ${offerDetails.title}\nDescription: ${offerDetails.description}\nPromo Code: ${offerDetails.promoCode}\nDiscount: ${offerDetails.persentage}%\nStart Date: ${offerDetails.startDate}\nEnd Date: ${offerDetails.dueDate}\nYou can visit our website through this link homelytech.lk\n\nBest regards,\nHomelyTech`
+            };
+
+            transporter.sendMail(mailOptions, (error, info) => {
+                if (error) {
+                    console.error("Error sending email: ", error);
+                } else {
+                    console.log("Email sent: " + info.response);
+                }
+            });
+        });
+    } catch (error) {
+        console.error("Error fetching users: ", error);
+    }
+};
+// Send email for specific offer
+const sendMailForOffer = async (req, res, next) => {
+    const offerId = req.params.id;
+
+    if (!mongoose.Types.ObjectId.isValid(offerId)) {
+        return res.status(400).json({ message: "Invalid offer ID" });
+    }
+
+    let offerDetails;
+
+    try {
+        offerDetails = await offer.findById(offerId);
+        if (!offerDetails) {
+            return res.status(404).json({ message: "Offer not found" });
+        }
+        await sendMailToUsers(offerDetails);
+        return res.status(200).json({ message: "Emails sent successfully" });
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json({ message: "Error sending emails" });
+    }
+};
+
+
+
 
 exports.getAllOffers = getAllOffers;
 exports.createOffer = createOffer;
 exports.getOfferById = getOfferById;
 exports.updateOffer = updateOffer;
 exports.deleteOffer = deleteOffer;
+exports.sendMailForOffer = sendMailForOffer;
